8a36c8cf5180de00388fae6cda8b868825f56c62
Developer Douglas <dglsdeveloper@gmail.com>
2024-04-20 21:57:41 +0300

    Feat: Can get mood type list and key value dynamically on negative mood

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ if ($text == "") {
 
 	//Start screen
 	$response = "CON How exactly?\n";
+	$number = 1;
 
 	foreach ($sql as $key => $value) {
 		# code...
@@ -34,8 +35,7 @@ if ($text == "") {
 		$mood_type = $value['mood_type'];
 
 		//Screen options
-		$response .= "$id. '.$mood_type.'\n";		
-	}
+		$response .= $number++ . ". $mood_type\n";	}
 
 	//Log results
 		$inslog = "INSERT INTO applogs(phone,session,mood,mood_type,verse,date_created) VALUES ('$phoneNumber','$sessionId','Positive','$mood_type','','$idate')";
